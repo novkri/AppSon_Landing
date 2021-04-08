056c281814b24d046e2ce054efa62c47cbef7104
novkri <nov.kri@yandex.ru>
2021-04-08 10:34:20 +0700

    slick slider

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.owl-carousel').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      swipe: true,
+      dots: true,
+      arrows: false,
+      // adaptiveHeight: true,
+      // mobileFirst: true,
+      // respondTo: min,
+      // rows: 1,
+      // variableWidth: true
+
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 550,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+});
